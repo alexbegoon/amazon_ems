95bf1053f5ca3429b76f96f3c28a9eb8058b5355
Alexander Begoon <alexbassmusic@gmail.com>
2013-11-26 20:43:24 +0400

    #224 added new statuses created SQL installation

--- application/helpers/my_form_helper.php
+++ application/helpers/my_form_helper.php
@@ -12,7 +12,49 @@ if ( ! function_exists('getStatusOptions'))
 	function getStatusOptions($selected = null)
 	{
 		// Statuses
-                $statuses = array('PREPARADO','NO','SI','INCIDENCIA','PREPARACION_ENGELSA_GLS','PREPARACION_ENGELSA_FEDEX','PEDIDO_ENGELSA_GLS','PEDIDO_ENGELSA_FEDEX','ENVIADO_GLS','ENVIADO_FEDEX','PEDIDO_ENGELSA_GLS_AQUI','PEDIDO_ENGELSA_FEDEX_AQUI','ROTURASTOCK','CANCELADO','ENVIADO_GRUTINET','ENALMACEN','PROCESADO_MEGASUR','ENVIADO_MEGASUR','PEDIDO_MARABE','ENVIADO_MARABE','MULTIPRODUCTO','PAGADO','PEDIDO_MARABE_AQUI','PAYPAL','PEDIDO_ENGELSA_PACK','PREPARACION_ENGELSA_PACK','PEDIDO_ENGELSA_PACK_AQUI','ENVIADO_PACK','PREPARACION_ENGELSA_TOURLINE','PREPARACION_ENGELSA_TOURLINE_AQUI','PEDIDO_ENGELSA_TOURLINE','ENVIADO_TOURLINE','PREPARACION_FARMA_TOURLINE','PREPARACION_FARMA_TOURLINE_AQUI','PEDIDO_FARMA_TOURLINE','PTE_PAGO');
+                $statuses = array(
+                    'PREPARADO',
+                    'NO',
+                    'SI',
+                    'INCIDENCIA',
+                    'PREPARACION_ENGELSA_GLS',
+                    'PREPARACION_ENGELSA_FEDEX',
+                    'PEDIDO_ENGELSA_GLS',
+                    'PEDIDO_ENGELSA_FEDEX',
+                    'ENVIADO_GLS',
+                    'ENVIADO_FEDEX',
+                    'PEDIDO_ENGELSA_GLS_AQUI',
+                    'PEDIDO_ENGELSA_FEDEX_AQUI',
+                    'ROTURASTOCK',
+                    'CANCELADO',
+                    'ENVIADO_GRUTINET',
+                    'ENALMACEN',
+                    'PROCESADO_MEGASUR',
+                    'ENVIADO_MEGASUR',
+                    'PEDIDO_MARABE',
+                    'ENVIADO_MARABE',
+                    'MULTIPRODUCTO',
+                    'PAGADO',
+                    'PEDIDO_MARABE_AQUI',
+                    'PAYPAL',
+                    'PEDIDO_ENGELSA_PACK',
+                    'PREPARACION_ENGELSA_PACK',
+                    'PEDIDO_ENGELSA_PACK_AQUI',
+                    'ENVIADO_PACK',
+                    'PREPARACION_ENGELSA_TOURLINE',
+                    'PREPARACION_ENGELSA_TOURLINE_AQUI',
+                    'PEDIDO_ENGELSA_TOURLINE',
+                    'ENVIADO_TOURLINE',
+                    'PREPARACION_FARMA_TOURLINE',
+                    'PREPARACION_FARMA_TOURLINE_AQUI',
+                    'PEDIDO_FARMA_TOURLINE',
+                    'PTE_PAGO',
+                    'PTE_ENVIO_GLS',
+                    'PTE_ENVIO_FEDEX',
+                    'PTE_ENVIO_PACK',
+                    'PTE_ENVIO_TOURLINE',
+                    'PEDIDO_ENGELSA_TOURLINE_AQUI'
+                    );
                 asort($statuses);
                 
                 echo '<option value=""></option>';
