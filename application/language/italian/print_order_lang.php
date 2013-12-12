c0276fcdc97fb40cf138a39017f86543ad71e4d5
Alexander Begoon <alexbassmusic@gmail.com>
2013-12-12 15:42:47 +0400

    #280
    Installed a new active language

---
+++ application/language/italian/print_order_lang.php
@@ -0,0 +1,25 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Print order language file
+ * 
+ * 
+ */
+
+$lang['print_order_sku']                    = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_customer_name']          = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_units']                  = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_shipping_address']       = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_zip_code']               = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_city']                   = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_product_label']          = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_id']                     = 'PLEASE FILL ITALIAN FILE';
+//$lang['print_order_payment_method_label']   = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_country_label']          = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_packing_slip']           = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_footer']                 = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_price']                  = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_subtotal_price']         = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_total']                  = 'PLEASE FILL ITALIAN FILE';
+$lang['print_order_shipping_fee']           = 'PLEASE FILL ITALIAN FILE';
+//$lang['print_order_']                  = '';
