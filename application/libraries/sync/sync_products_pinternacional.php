a0f05d5299beb6e969d4b5042646c43c83ee9fa9
Alexander Begoon <alexbassmusic@gmail.com>
2014-01-16 16:52:34 +0400

    #346

--- application/libraries/sync/sync_products_pinternacional.php
+++ application/libraries/sync/sync_products_pinternacional.php
@@ -56,7 +56,7 @@ class Sync_products_pinternacional extends Sync_products
                 $this->_products[$i]['product_name'] = trim($product[0]);
                 $this->_products[$i]['provider_name'] = $this->_provider_name;
                 $this->_products[$i]['price']   = (float)$product[2];
-                if(in_array((string)$this->_products[$i]['sku'], $eans_to_exclude))
+                if(in_array((string)$this->_products[$i]['sku'], $eans_to_exclude) || (int)$product[3] <= 3)
                 {
                     $this->_products[$i]['stock'] = 0;
                 }
