c44aa11e9e34ffe260024b533294e70bb069388e
Alexander Begoon <alexbassmusic@gmail.com>
2014-03-03 22:03:27 +0400

    #426

--- application/libraries/sync/sync_products_coqueteo.php
+++ application/libraries/sync/sync_products_coqueteo.php
@@ -13,7 +13,7 @@ class Sync_products_coqueteo extends Sync_products
     {
         parent::__construct();
         
-        $this->_url_service = 'http://www.buyin.es/ficherocoqueteo/fichero.csv';
+        $this->_url_service = 'http://localhost/ficherocoqueteo/fichero.csv';
         $this->_provider_name = 'COQUETEO';
         
         // Test mode toggle
