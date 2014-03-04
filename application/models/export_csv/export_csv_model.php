3eebb42597474e0b2b30b88a867864c3e02082ed
Alexander Begoon <alexbassmusic@gmail.com>
2014-03-04 15:32:47 +0400

    #426

--- application/models/export_csv/export_csv_model.php
+++ application/models/export_csv/export_csv_model.php
@@ -865,6 +865,10 @@ class Export_csv_model extends CI_Model
         foreach ($header as $cell)
         {
             $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($i, 1, $cell);
+            $objPHPExcel->getActiveSheet()->getStyleByColumnAndRow($i, 1)->getFill()
+            ->applyFromArray(array('type' => PHPExcel_Style_Fill::FILL_SOLID,
+            'startcolor' => array('rgb' => 'c3c3c3')
+            ));
             $i++;
         }
         
