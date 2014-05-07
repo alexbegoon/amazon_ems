0ef4d79f5aedb1c1b6b4192d7d24418e7c0639b0
Alexander Begoon <alexbassmusic@gmail.com>
2014-05-07 18:27:01 +0400

    Revert "#525"
    This reverts commit d322ab285db9b8fc1c8333fb8851986026543aa4.
    
    Conflicts:
    	application/libraries/sync/sync_products_pinternacional.php

--- application/libraries/sync/sync_products_pinternacional.php.BASE.php
+++ application/libraries/sync/sync_products_pinternacional.php.BASE.php
@@ -13,7 +13,8 @@ class Sync_products_pinternacional extends Sync_products
     {
         parent::__construct();
         
-        $this->_url_service = 'http://www.perfumeriainternacional.infor-tel.com/pinter/tarifas.txt';
+//        $this->_url_service = 'http://www.perfumeriainternacional.infor-tel.com/pinter/tarifas.txt';
+        $this->_uri_service = 'http://88.12.29.141:8101/SOAP';
         $this->_provider_name = 'PINTERNACIONAL';
         
         // Test mode toggle
@@ -28,7 +29,7 @@ class Sync_products_pinternacional extends Sync_products
     
     protected function extract_products() 
     {        
-        $data_file = file_get_contents($this->_url_service);
+        $data_file = $this->get_data_from_service();
         
         if(!$data_file)
         {
@@ -46,7 +47,7 @@ class Sync_products_pinternacional extends Sync_products
         
         foreach ($data_array as $row)
         {
-            $product = explode("\t", $row);
+            $product = explode(";", $row);
             
             if($this->_test_mode)
             {
@@ -55,22 +56,22 @@ class Sync_products_pinternacional extends Sync_products
                 echo '<pre>';
             }
             
-            if(isset($product[4]))
+            if(isset($product[1]) && !empty($product[1]))
             {
-                $this->_products[$i]['sku'] = trim($product[4]);
-                $this->_products[$i]['product_name'] = trim($product[0]);
+                $this->_products[$i]['sku'] = trim($product[1]);
+                $this->_products[$i]['product_name'] = trim($product[2]);
                 $this->_products[$i]['provider_name'] = $this->_provider_name;
-                $this->_products[$i]['price']   = (float)$product[2];
-                if(in_array((string)$this->_products[$i]['sku'], $this->_eans_to_exclude) || (int)$product[3] <= 3)
+                $this->_products[$i]['price']   = (float)str_replace(',', '.', $product[5]);
+                if(in_array((string)$this->_products[$i]['sku'], $this->_eans_to_exclude) || (int)$product[6] <= 3)
                 {
                     $this->_products[$i]['stock'] = 0;
                 }
                 else
                 {
-                    $this->_products[$i]['stock']   = (int)$product[3];
+                    $this->_products[$i]['stock']   = (int)$product[6];
                 }
-                $this->_products[$i]['brand']   = trim($product[5]);
-                $this->_products[$i]['sex']     = trim($product[7]);
+                $this->_products[$i]['brand']   = trim($product[4]);
+                $this->_products[$i]['sex']     = trim($product[3]);
             }
             
             if($this->_test_mode)
@@ -109,5 +110,26 @@ class Sync_products_pinternacional extends Sync_products
         $this->_eans_to_exclude = $eans_to_exclude;
         
         return TRUE;
+    }
+    
+    private function get_data_from_service()
+    {
+        $client = new SoapClient($this->_uri_service);
+        
+        $test = $client->__soapCall('ExecuteMapping', array(
+                                            'ICGDATAEXCHANGE.EXE 6 SERVICE   -PARAMVALUE CLIENTE=3645 -PARAMVALUE MARCA=TODAS -PARAMVALUE CODBARRAS=TODOS',
+                                            '',
+                                            '',
+                                            '',
+                                                    
+                                        ) );
+        
+        if (!empty($test['MsgError']))
+        {
+            log_message('ERROR', $test['MsgError']);
+            log_message('INFO', $test['Info']);
+        }
+        
+        return $test['Datos'];
     }
 }
