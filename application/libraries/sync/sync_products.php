039bdb2b56089dc6748ff5dacb0f67cfbb4f4a7b
Alexander Begoon <alexbassmusic@gmail.com>
2014-09-16 20:05:36 +0400

    #721

--- application/libraries/sync/sync_products.php
+++ application/libraries/sync/sync_products.php
@@ -79,7 +79,7 @@ abstract class Sync_products
         //Find duplicates        
         foreach ($products as $product)
         {
-            isset($skus["{$product['sku']}"])?$skus["{$product['sku']}"]+=1:$skus["{$product['sku']}"]=1;
+            isset($skus[$product['sku']])?$skus[$product['sku']]+=1:$skus[$product['sku']]=1;
         }
         $duplicates = array_filter(
         $skus,
@@ -87,6 +87,7 @@ abstract class Sync_products
             return ($value >= 2);
         }
         );
+        var_dump($duplicates);die;
         //Process duplicates
         foreach ($duplicates as $duplicate_sku=>$v)
         {
@@ -94,12 +95,12 @@ abstract class Sync_products
             {
                 if($product['sku']===$duplicate_sku)
                 {
-                    $duplicate_products["{$product['sku']}"][$k] = $product;
+                    $duplicate_products[$product['sku']][$k] = $product;
                 }
             }
         }
         
-            var_dump($duplicate_products);die;
+            
         $ids_to_exclude = array();
         $total_stock = array();
         $final_price = array();
